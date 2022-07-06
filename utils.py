0b79c319372d4dbab8acacaf0dbfac1fa07e5f6d
NGUYENSO <sophia.nguyen@mpac.ca>
2022-07-06 09:56:38 -0400

    update comment

--- utils.py
+++ utils.py
@@ -1,22 +1,22 @@
 import ijson
 
-'''
-customized from ijson.parse function to return more descriptive prefixes for arrays
-
-eg.
-{
-    "a" : ["one", "two", "three"]
-}
-the value "one" would have the prefix `a.0` instead of `a.item` as ijson.parse would give.
-Similarly, "two" would have the prefix `a.1` and "three" `a.2`.
-'''
 
 def parse(f):
+    '''
+    customized from ijson.parse function to return more descriptive prefixes for arrays
+
+    eg.
+    {
+        "a" : ["one", "two", "three"]
+    }
+    the value "one" would have the prefix `a.0` instead of `a.item` as ijson.parse would give.
+    Similarly, "two" would have the prefix `a.1` and "three" `a.2`.
+    '''
 
     basic_events = ijson.basic_parse(f, multiple_values=True)
     path = []
-    arr_indices = [] # tracking indices of array elements in json
-    current_i = None # index of current json array element index in the path
+    arr_indices = []  # tracking indices of array elements in json
+    current_i = None  # index of current json array element index in the path
     # purpose of current_i is mainly to differentiate between regular arrays [1,2,3] and arrays with nested json in them
 
     for event, value in basic_events:
@@ -49,10 +49,8 @@ def parse(f):
 
         else:  # any scalar value
             prefix = '.'.join(path)
-            if arr_indices and current_i == len(path) - 1: # if array is of type [value1, value2, value3]
+            if arr_indices and current_i == len(path) - 1:  # if array is of type [value1, value2, value3]
                 arr_indices[-1] = arr_indices[-1] + 1
-                path[current_i] = str(arr_indices[-1]) # update the path
-
-
+                path[current_i] = str(arr_indices[-1])  # update the path
 
         yield prefix, event, value
