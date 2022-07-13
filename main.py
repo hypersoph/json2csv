37ea4cb4b1f20d323f65f69eb1e7d913cb718a96
NGUYENSO <sophia.nguyen@mpac.ca>
2022-07-13 16:28:29 -0400

    generate only specific tables

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ OUT_DIR = Path("output")  # modify this to desired output directory
 IDENTIFIERS = ["factId", "rollNumber"]
 CHUNK_SIZE = 10000
 
-def create_mappings():
+
+def create_mappings(select_tables):
     """
     - only needs to be done once for the entire program
     - assumes that every json has the same top-level keys
@@ -24,10 +25,11 @@ def create_mappings():
 
     with open(JSON_FILE, "r") as f:
         # First pass: add all top-level keys using first json in file
-        for (_, prefix, event, value) in parse(f):
-            if prefix == '' and event == 'map_key' and value not in IDENTIFIERS:
+        for (_, prefix, event, value) in parse(f, multiple_values=True):
+            if not select_tables and prefix == '' and event == 'map_key' and value not in IDENTIFIERS:
                 mappings[value] = {}
-
+            elif prefix == '' and event == 'map_key' and value not in IDENTIFIERS and value in select_tables:
+                mappings[value] = {}
             elif prefix == '' and event == 'end_map' and value is None:
                 # first pass done, initiate second pass
                 f.seek(0)  # read from beginning again
@@ -40,7 +42,7 @@ def create_mappings():
 
         # Second pass: add all column names to mappings with default values
         # This pass goes through the entire json file to collect all possible columns
-        for (base_prefix, prefix, event, value) in parse(f):
+        for (base_prefix, prefix, event, value) in parse(f, multiple_values=True):
             if event == "string" or event == "number":
                 # find table that matches the prefix and add value if value is an external node
                 if base_prefix in list(mappings.keys()):
@@ -54,12 +56,14 @@ def create_mappings():
     return mappings
 
 
-def json_flat(mappings, writers):
+def json_flat(mappings, writers, select_tables):
     """
+    Flatten json
 
-    :param mappings:
-    :param writers:
-    :return:
+    :param select_tables:
+    :param mappings: mapping dict specifying structure of output files
+    :param writers: list of output writers
+    :return: count of json lines
     """
     count_rows = 0  # track number of rows written
     row_collector = defaultdict(list)
@@ -71,53 +75,51 @@ def json_flat(mappings, writers):
         roll_number = None
         fact_id = None
 
-        for (base_prefix, prefix, event, value) in parse(jsonfile):  # without multiple_values flag our json is invalid
+        for (base_prefix, prefix, event, value) in parse(jsonfile,
+                                                         multiple_values=True):  # without multiple_values flag our json is invalid
 
             if roll_number is None and base_prefix == "rollNumber":
                 roll_number = value
             if fact_id is None and base_prefix == "factId":
                 fact_id = value
 
-            if base_prefix not in IDENTIFIERS:
-                # if leaf reached and the field is not yet populated, set the value
-                if (event == "string" or event == "number") and mappings[base_prefix][prefix] is None:
-
-                    if mappings[base_prefix][prefix] is None:  # value for key prefix empty
+            if event == "string" or event == "number":
+                if base_prefix in select_tables and base_prefix not in IDENTIFIERS:
+                    # if leaf reached and the field is not yet populated, set the value
+                    if mappings[base_prefix][prefix] is None:
                         mappings[base_prefix][prefix] = value
-                # else if leaf reached and field is already populated, append to array
-                elif event == "string" or event == "number":  # value for key prefix already populated, add to/create an array
 
-                    # Note: growing arrays is an expensive operation, may look into linked lists or other solution
-                    if type(mappings[base_prefix][prefix]) == list:
+                    # else if leaf reached and field is already populated, create or append to array
+                    elif type(mappings[base_prefix][prefix]) == list:
                         mappings[base_prefix][prefix] = [*mappings[base_prefix][prefix],
                                                          value]  # unpack existing array into new one
                     else:
                         mappings[base_prefix][prefix] = [mappings[base_prefix][prefix], value]
 
-                # if reached end of a top-level json (ie. finished one property)
-                elif prefix == '' and event == 'end_map' and value is None:
-                    count_rows = count_rows + 1
+            # if reached end of a top-level json (ie. finished one property)
+            elif prefix == '' and event == 'end_map' and value is None:
+                count_rows = count_rows + 1
 
-                    # add roll number and fact id to row
-                    for table in mappings:
-                        mappings[table]['rollNumber'] = roll_number
-                        mappings[table]['factId'] = fact_id
-                        row = mappings[table].maps[0].copy()  # append copy so that row doesn't get reset
-                        row_collector[table].append(row)
+                # add roll number and fact id to row
+                for table in mappings:
+                    mappings[table]['rollNumber'] = roll_number
+                    mappings[table]['factId'] = fact_id
+                    row = mappings[table].maps[0].copy()  # append copy so that row doesn't get reset
+                    row_collector[table].append(row)
 
-                        # reset map
-                        mappings[table].maps[0].clear()
+                    # reset map
+                    mappings[table].maps[0].clear()
 
-                    # write all collected rows if num rows exceeds specified size
-                    if len(row_collector) >= CHUNK_SIZE:
-                        for writer, rows in zip(writers, row_collector):
-                            writer.writerows(row_collector[rows])
+                # write all collected rows if num rows exceeds specified size
+                if len(row_collector) >= CHUNK_SIZE:
+                    for writer, rows in zip(writers, row_collector):
+                        writer.writerows(row_collector[rows])
 
-                        row_collector = defaultdict(list)
+                    row_collector = defaultdict(list)
 
-                    # reset variables
-                    roll_number = None
-                    fact_id = None
+                # reset variables
+                roll_number = None
+                fact_id = None
 
     # write any remaining rows
     if row_collector:
@@ -128,7 +130,10 @@ def json_flat(mappings, writers):
 
 
 def main():
-    mappings = create_mappings()
+    select_tables = ["improvements", "site"]
+    mappings = create_mappings(select_tables)
+
+    select_tables = list(mappings.keys()) if not select_tables else select_tables
 
     # create output directory if does not exist
     if not os.path.exists(OUT_DIR):
@@ -146,7 +151,7 @@ def main():
 
     start = time.time()  # track overall run time of flattening algorithm
 
-    count_rows = json_flat(mappings, writers)
+    count_rows = json_flat(mappings, writers, select_tables)
     out_files.close()  # important - must close output files
 
     end = time.time()
