6894a272388a369cfa1fa7228f5ed66a73d0459c
NGUYENSO <sophia.nguyen@mpac.ca>
2022-07-27 16:06:45 -0400

    Revert "fixes, comments"
    This reverts commit 7c55fbe4fc1faa167a06c0bfcca1fcb15778d429.

--- helpers.py
+++ helpers.py
@@ -57,7 +57,7 @@ class RowBuffer:
 
 class FileHandler:
     """
-    Helps with file operations on many files at once
+    Represents a dict of all CSV files with methods to open and close all
     """
 
     def __init__(self):
